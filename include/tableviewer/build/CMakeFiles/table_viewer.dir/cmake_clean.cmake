94a058dc7035de0a80bbf1a5248c0a3865abd2ee
zzzzzzzzZz-chow <1416271215@qq.com>
2024-02-05 17:34:54 +0800

    新增tabelviewer

---
+++ include/tableviewer/build/CMakeFiles/table_viewer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/table_viewer.dir/table_viewer.cpp.o"
+  "CMakeFiles/table_viewer.dir/table_viewer.cpp.o.d"
+  "CMakeFiles/table_viewer.dir/test.cpp.o"
+  "CMakeFiles/table_viewer.dir/test.cpp.o.d"
+  "table_viewer"
+  "table_viewer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/table_viewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- include/tableviewer/test.cpp
+++ include/tableviewer/test.cpp
@@ -1,13 +1,20 @@
 #include "table_viewer.hpp"
 #include <string>
 
+consteval int f() { return 2; }
+constinit int a = f();
+
 int main() {
+  constexpr int a = f();
   TableViewer t;
   t.set_print_style(
       "what", [](std::any a) { return std::to_string(std::any_cast<int>(a)); });
   t.add_value("what", 123);
   t.set_print_style("赵宇轩", [](std::any a) {
-    return std::to_string(std::any_cast<double>(a));
+    std::string s = std::to_string(std::any_cast<double>(a));
+    s.resize(6);
+    s += "%";
+    return s;
   });
   t.add_value("赵宇轩", 1.0);
   t.add_value("赵宇轩", 1.0);
